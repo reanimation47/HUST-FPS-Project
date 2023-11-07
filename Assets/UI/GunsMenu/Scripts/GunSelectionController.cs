d2c1530e048cc75d4ec2b396e19d6766ff6932a6
reanimation47 <33254512+reanimation47@users.noreply.github.com>
2023-11-07 14:00:06 +0700

    add stuff

--- Assets/UI/GunsMenu/Scripts/GunSelectionController.cs
+++ Assets/UI/GunsMenu/Scripts/GunSelectionController.cs
@@ -6,19 +6,9 @@ using UnityEngine.UI;
 public class GunSelectionController : MonoBehaviour
 {
     public GameObject[] GunsList;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private int currentGunIndex = -1;
+
     public void NextGun()
     {
         //Disable all guns
@@ -28,7 +18,6 @@ public class GunSelectionController : MonoBehaviour
         }
         currentGunIndex += 1;
         GunsList[currentGunIndex].SetActive(true);
-        
     }
 
 }
