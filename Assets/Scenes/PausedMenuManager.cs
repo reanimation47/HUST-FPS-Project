30c77868aa487e0ee3fc0c9af76bfbcf51394fdc
quangthanghust <thang.tq207701@sis.hust.edu.vn>
2023-12-17 18:13:47 +0700

    update Paused code trong Player va them Pausebackground vao SampleScene

---
+++ Assets/Scenes/PausedMenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausedMenuManager : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    [SerializeField] private GameObject pausedMenu;
+    
+    private void Start()
+    {
+        ActivatePausedMenu(true);
+
+    }
+
+    private void update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+             ActivatePausedMenu(true);
+        else if(Input.GetKeyDown(KeyCode.Escape) && isPaused)
+             ActivatePausedMenu(false);    
+    }
+
+    public void ActivatePausedMenu(bool state)
+    {
+        pausedMenu.SetActive(state);
+
+        Time.timeScale = state ? 0 : 1;
+        isPaused = state;
+    }
+}
