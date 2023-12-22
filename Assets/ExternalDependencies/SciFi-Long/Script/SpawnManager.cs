070107fbadbab46dda6b55cda5448cec83257654
reanimation47 <33254512+reanimation47@users.noreply.github.com>
2023-12-22 22:49:31 +0700

    update to work for both single player + multiplayer

--- Assets/ExternalDependencies/SciFi-Long/Script/SpawnManager.cs
+++ Assets/ExternalDependencies/SciFi-Long/Script/SpawnManager.cs
@@ -26,11 +26,11 @@ namespace Player {
         return SpawnPoints[Random.Range(0, SpawnPoints.Length)];
     }
 
-    //public void RespawnSelf(GameObject target)
-    //{
-    //    target.SetActive(false);
-    //    target.transform.position = GetSpawnPoint().position;
-    //    target.SetActive(true);
-    //}
+    public void RespawnSelf(GameObject target)
+    {
+       target.SetActive(false);
+       target.transform.position = GetSpawnPoint().position;
+       target.SetActive(true);
+    }
 }
  }
