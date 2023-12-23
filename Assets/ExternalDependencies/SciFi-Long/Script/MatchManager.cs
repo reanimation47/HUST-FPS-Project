c81ebd276a01b38966eaac983acb9c67452bbf29
LongNguyenVu24 <long.nv207687@sis.hust.edu.vn>
2023-12-23 13:40:39 +0700

    Update MatchManeger
    for st that is relevant to leaderboard logic

--- Assets/ExternalDependencies/SciFi-Long/Script/MatchManager.cs
+++ Assets/ExternalDependencies/SciFi-Long/Script/MatchManager.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
-public class MatchManager : MonoBehaviour
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+using UnityEngine.InputSystem.XR;
+
+public class MatchManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     public static MatchManager instance;
 
@@ -12,18 +16,244 @@ public class MatchManager : MonoBehaviour
         instance = this;
     }
 
-    void Start()
+    public List<PlayerInfo> allPlayers = new List<PlayerInfo>();
+    private int index;
+    public GameState state = GameState.Waiting;
+
+    public enum EventCodes : byte
     {
-        
+        NewPlayer,
+        ListPlayers,
+        UpdateStat,
+        NextMatch,
+        TimerSync
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum GameState
+    {
+        Waiting,
+        Playing,
+        Ending
+    }
+
+
+    void Start()
     {
         if (!PhotonNetwork.IsConnected)
         {
             SceneManager.LoadScene(0);
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
 
     }
+    public void NewPlayerSend(string username)
+    {
+        object[] package = new object[4];
+        package[0] = username;
+        package[1] = PhotonNetwork.LocalPlayer.ActorNumber;
+        package[2] = 0;
+        package[3] = 0;
+
+
+        PhotonNetwork.RaiseEvent(
+            (byte)EventCodes.NewPlayer,
+            package,
+            new RaiseEventOptions { Receivers = ReceiverGroup.MasterClient },
+            new SendOptions { Reliability = true }
+            );
+    }
+
+    public void NewPlayerReceive(object[] dataReceived)
+    {
+        PlayerInfo player = new PlayerInfo((string)dataReceived[0], (int)dataReceived[1], (int)dataReceived[2], (int)dataReceived[3]);
+
+        allPlayers.Add(player);
+
+        ListPlayersSend();
+    }
+
+    public void UpdateStatsSend(int actorSending, int statToUpdate, int amountToChange)
+    {
+        object[] package = new object[] { actorSending, statToUpdate, amountToChange };
+
+        PhotonNetwork.RaiseEvent(
+            (byte)EventCodes.UpdateStat,
+            package,
+            new RaiseEventOptions { Receivers = ReceiverGroup.All },
+            new SendOptions { Reliability = true }
+            );
+    }
+
+    public void UpdateStatsReceive(object[] dataReceived)
+    {
+        int actor = (int)dataReceived[0];
+        int statType = (int)dataReceived[1];
+        int amount = (int)dataReceived[2];
+
+        for (int i = 0; i < allPlayers.Count; i++)
+        {
+            if (allPlayers[i].actor == actor)
+            {
+                switch (statType)
+                {
+                    case 0: //kills
+                        allPlayers[i].kills += amount;
+                        Debug.Log("Player " + allPlayers[i].name + " : kills " + allPlayers[i].kills);
+                        break;
+
+                    case 1: //deaths
+                        allPlayers[i].deaths += amount;
+                        Debug.Log("Player " + allPlayers[i].name + " : deaths " + allPlayers[i].deaths);
+                        break;
+                }
+
+           /*     if (i == index)
+                {
+                    UpdateStatsDisplay();
+                }
+
+                if (UIController.instance.leaderboard.activeInHierarchy)
+                {
+                    ShowLeaderboard();
+                }
+*/
+                break;
+            }
+        }
+
+       // ScoreCheck();
+    }
+
+
+    public void ListPlayersSend()
+    {
+        object[] package = new object[allPlayers.Count + 1];
+
+        package[0] = state;
+
+        for (int i = 0; i < allPlayers.Count; i++)
+        {
+            object[] piece = new object[4];
+
+            piece[0] = allPlayers[i].name;
+            piece[1] = allPlayers[i].actor;
+            piece[2] = allPlayers[i].kills;
+            piece[3] = allPlayers[i].deaths;
+
+            package[i + 1] = piece;
+        }
+
+        PhotonNetwork.RaiseEvent(
+            (byte)EventCodes.ListPlayers,
+            package,
+            new RaiseEventOptions { Receivers = ReceiverGroup.All },
+            new SendOptions { Reliability = true }
+            );
+    }
+
+    public void ListPlayersReceive(object[] dataReceived)
+    {
+        allPlayers.Clear();
+
+        state = (GameState)dataReceived[0];
+
+        for (int i = 1; i < dataReceived.Length; i++)
+        {
+            object[] piece = (object[])dataReceived[i];
+
+            PlayerInfo player = new PlayerInfo(
+                (string)piece[0],
+                (int)piece[1],
+                (int)piece[2],
+                (int)piece[3]
+                );
+
+            allPlayers.Add(player);
+
+            if (PhotonNetwork.LocalPlayer.ActorNumber == player.actor)
+            {
+                index = i - 1;
+            }
+        }
+
+        StateCheck();
+    }
+    public void OnEvent(EventData photonEvent)
+    {
+        if(photonEvent.Code < 200)
+        {
+            EventCodes theEvent = (EventCodes)photonEvent.Code;
+            object[] data = (object[])photonEvent.CustomData;
+            switch (theEvent)
+            {
+                case EventCodes.NewPlayer:
+
+                    NewPlayerReceive(data);
+
+                    break;
+
+                case EventCodes.ListPlayers:
+
+                    ListPlayersReceive(data);
+
+                    break;
+
+                case EventCodes.UpdateStat:
+
+                    UpdateStatsReceive(data);
+
+                    break;
+
+                case EventCodes.NextMatch:
+
+                   // NextMatchReceive();
+
+                    break;
+
+                case EventCodes.TimerSync:
+
+                    //TimerReceive(data);
+
+                    break;
+            }
+        }
+    }
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    void StateCheck()
+    {
+
+    }
+    #region PlayerInfo
+
+    [System.Serializable]
+    public class PlayerInfo
+    {
+        public string name;
+        public int actor, kills, deaths;
+
+
+
+        public PlayerInfo(string _name, int _actor, int _kills, int _deaths)
+        {
+            name = _name;
+            actor = _actor;
+            kills = _kills;
+            deaths = _deaths;
+        }
+    }
+    #endregion
 }
