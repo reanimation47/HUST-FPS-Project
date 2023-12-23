3b1ac226e36521c0c4a6a8c5d8dcb48b52828b9c
LongNguyenVu24 <long.nv207687@sis.hust.edu.vn>
2023-12-23 21:53:39 +0700

    update
    ui config getting some scene : menu, pause, select map , pvp, pve

---
+++ Assets/UI/PausedMenu/PausedMenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+public class PausedMenuManager : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    [SerializeField] private GameObject pausedMenu;
+    
+    private void Start()
+    {
+        ActivatePausedMenu(false);
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+             ActivatePausedMenu(true);
+        else if(Input.GetKeyDown(KeyCode.Escape) && isPaused)
+             ActivatePausedMenu(false);    
+    }
+
+    public void ActivatePausedMenu(bool state)
+    {
+        pausedMenu.SetActive(state);
+
+        Time.timeScale = state ? 0 : 1;
+        isPaused = state;
+    }
+    
+//    public void Restart()
+//    {
+//        SceneManagement.LoadScene("SampleScene");
+//    }
+    
+//      public void Option()
+//      {
+//        SceneManagement.LoadScene("Option");
+//      }
+
+//    public void BackToMenu()
+//    {
+//        SceneManagement.LoadScene("MainMenu");
+//    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+}
