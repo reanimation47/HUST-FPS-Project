6c2a22a804b0337b81aa389e4d64e477b6e5857b
Haniecute <107951247+NguyenHanh20198295@users.noreply.github.com>
2024-01-03 13:52:46 +0700

    ...

---
+++ Assets/UI/MainMenu-Revamp/ScenesDatabase/SceneNames.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class SceneNames 
+{
+    public readonly string MainMenu = "MainMenu";
+    public readonly string PVP_Lobby = "Multiplayer-PVP-Lobby";
+    public readonly string PvE_MapSelect = "PvEMapSelect";
+    public readonly string WeaponsShop = "WeaponsShop";
+}
+
+public class PvESceneNames
+{
+    public readonly string Map1 = "PvE-Map1";
+    public readonly string Map2 = "PvE-Map2";
+}
